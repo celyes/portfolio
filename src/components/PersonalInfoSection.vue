16353fbb411d30e7155b582cc93d858c153dcb59
celyes <celyes02@gmail.com>
2023-10-18 14:56:11 +0200

    fix columns for personal info section

--- src/components/PersonalInfoSection.vue
+++ src/components/PersonalInfoSection.vue
@@ -17,7 +17,7 @@
         </div>
     </div>
     <div class="row pt-4">
-        <div class="col-sm-6my-2">
+        <div class="col-sm-6 my-2">
         <span class="d-flex justify-content-center mx-3">
           <a rel="noopener" href="mailto:celyes02@gmail.com"
              class="text-muted contact">
