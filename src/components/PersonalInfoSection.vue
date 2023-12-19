0fa8b2b099d5c02273670d9cc022e4cc78c706f0
Ilyes Chouia <celyes02@gmail.com>
2023-12-19 12:03:30 +0100

    Update PersonalInfoSection.vue

--- src/components/PersonalInfoSection.vue
+++ src/components/PersonalInfoSection.vue
@@ -10,7 +10,7 @@
             </div>
             <div class="d-flex justify-content-center mt-5">
                 <h2 class="text-muted job">
-                    Backend Engineer
+                    Fullstack software Engineer
                 </h2>
             </div>
             <br/>
